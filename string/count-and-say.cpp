676c35159e5b2b03a5299489154dcf31a77b7fd8
Nikhil-O3 <nikhilbagul2005@gmail.com>
2025-12-20 15:53:35 +0530

    optimized

--- string/count-and-say.cpp
+++ string/count-and-say.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    string rec(int n)
-    {
+   
+    string countAndSay(int n) {
         if(n==1)return "1";
 
-        string str=rec(n-1);
+        string str=countAndSay(n-1);
         string res="";
 
-        int i=0;
+        int i=0,count=0;
         while(i<str.size())
         {
-            int count=0;
+            count=0;
             char ch=str[i];
             while(i<str.size() && ch==str[i])
             {
                 count++;i++;
             }
-            res=res+char(count+'0')+ch;
+            res=res+to_string(count)+ch;
             
         }
 
         return res;
-    }
-    string countAndSay(int n) {
-        return rec(n);
     }
 };
